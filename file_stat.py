94b4eccd7bdfcce35401e09e49aabe0e6f817e4d
GinVlad <minhthanhuit@gmail.com>
2016-04-23 14:40:35 +0700

    Added file_stat.py (Using os.stat())

---
+++ file_stat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import os, sys
+
+file = os.stat(sys.argv[1])
+
+if file:
+	print "Filename: %s" % sys.argv[1]
+	print "File size: %d" % file.st_size
+	print "Owner by: %d" %file.st_uid
+else:
+	print "File not found"
